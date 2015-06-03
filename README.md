77a98e3edc818238bbebde0299ed6300dea8d7ed
OJIMA Ryoji <rojima1@gmail.com>
2015-06-03 22:38:24 +0900

    CUI spec is changed.

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # polyomino-enclosure
 
-A Clojure library designed to ... well, that part is up to you.
+A test environment for NUL programming coucours 2015 problem #2.
 
 ## Usage
 
-FIXME
+### Generating Clojure and ClojureScript from *.cljx.
+
+````shell
+$ lein cljx once
+````
+
+### Generating JavaScript from ClojureScript.
+
+````shell
+$ lein cljsbuild once
+````
+
+### Open `resources/public/index.html
+
+Push [Execute] button.
+
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 OJIMA Ryoji.
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the Eclipse Public License either version 1.0 or any later version.
--- src/polyomino_enclosure/core.clj
+++ src/polyomino_enclosure/core.clj
@@ -1,7 +1,6 @@
 (ns polyomino-enclosure.core
   (:use     (polyomino-enclosure engine))
-  (:require (clojure             [string :as string])
-            (clojure.java        [shell  :as shell]))
+  (:require (clojure             [string :as string]))
   (:gen-class))
 
 (defn- println-err
@@ -10,18 +9,11 @@
     (apply println more)))
 
 (defn -main
-  [question-file solution-program]
-  (let [question        (slurp question-file)
-        start-nano-time (System/nanoTime)
-        answer          (:out (shell/sh solution-program :in question))
-        end-nano-time   (System/nanoTime)
-        polyominos      (create-polyominos (string/split-lines question))
-        programs        (create-programs   (string/split-lines answer))]
-    (if-let [error (validate-game polyominos programs)]
-      (println-err error)
-      (let [result (result (execute-programs polyominos programs))]
-        (if-let [error (validate-result result)]
-          (println-err error)
-          (println (/ (double (- end-nano-time start-nano-time)) 1000000.0)
-                   (score result))))))
-  (shutdown-agents))
+  [question-file answer-file]
+  (let [polyominos (create-polyominos (string/split-lines (slurp question-file)))
+        programs   (create-programs   (string/split-lines (slurp answer-file)))]
+    (if-let [error (or (validate-game polyominos programs)
+                       (validate-result (result (execute-programs polyominos programs))))]
+      (do (println-err error)
+          (println "0"))
+      (println (score (result (execute-programs polyominos programs)))))))
